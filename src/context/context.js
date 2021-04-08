0a3b9d3028ac71f96353ff2e9c90bc8fe3248ea4
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-04-08 08:16:50 +0530

    Setting up UI with mock data

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react"
+import mockUser from "./mockData.js/mockUser"
+import mockRepos from "./mockData.js/mockRepos"
+import mockFollowers from "./mockData.js/mockFollowers"
+import axios from "axios"
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com"
+
+const GithubContext = React.createContext()
+
+const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser)
+  const [repos, setRepos] = useState(mockRepos)
+  const [followers, setFollowers] = useState(mockFollowers)
+
+  // Requests loading
+  const [requests, setRequests] = useState(0)
+  const [loading, setLoading] = useState(false)
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data
+        setRequests(remaining)
+        if (remaining === 0) {
+          //throw an error
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  // error
+
+  useEffect(checkRequests, [])
+
+  return (
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+      {children}
+    </GithubContext.Provider>
+  )
+}
+
+export { GithubProvider, GithubContext }
